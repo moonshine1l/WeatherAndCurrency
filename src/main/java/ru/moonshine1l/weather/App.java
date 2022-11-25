ebcac010cc8b0397e945910eda4089867c3172d6
moonshine1l <norka5imen@gmail.com>
2022-11-25 18:29:34 +0300

    Icon Update

--- src/main/java/ru/moonshine1l/weather/App.java
+++ src/main/java/ru/moonshine1l/weather/App.java
@@ -3,6 +3,7 @@ package ru.moonshine1l.weather;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("stage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 702, 588);
+        stage.getIcons().add(new Image("C:\\Users\\user\\IdeaProjects\\weather\\src\\main\\resources\\ru\\moonshine1l\\weather\\WeatherCat.png"));
         stage.setTitle("Погода");
         stage.setScene(scene);
         stage.show();
